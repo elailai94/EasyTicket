f6f20b7b8bcdb47cf733661ff0a3d2a1bb574bb8
Elisha Lai <elisha.lai@outlook.com>
2015-04-20 13:50:31 -0400

    Update station.py
    Added is_open field and additional methods to mutate field to Station class.

--- Source-Code/Station/station.py
+++ Source-Code/Station/station.py
@@ -3,7 +3,7 @@
 #
 # @description: Module for providing methods to work with Station objects
 # @author: Elisha Lai
-# @version: 1.2 16/04/2015
+# @version: 1.3 20/04/2015
 #==============================================================================
 
 # Station module (station.py)
@@ -11,32 +11,40 @@
 # Object definition
 class Station:
 
-    'Fields: name, zone, lines'
+    'Fields: name, is_open, zones, lines'
     # A Station is an object in which
     # - name is a Str (name of a station)
-    # - zone is an Int (zone that this station is located in)
+    # - is_open is a Bool (status indicating whether this station is open/close)
+    # - zones is a (listof Int) (zones that this station is located in)
     # - lines is a (listof Str) (lines that this station is part of)
 
     # Initializes the object.
-    def __init__(self, name, zone):
-	self.name = name
-	self.zone = zone
-	self.lines = []
+    def __init__(self, name):
+    	self.name = name
+	self.is_open = True
+	self.zones = set()
+	self.lines = set()
+
+    # Concatenates all items in a set s into one string
+    def set_to_string(self, s):
+    	# Stores the index counter
+    	i = 0
+    	# Stores the concatenated string to be returned
+    	ret_str = ''
+    	# Concatenates all items in the set s into one string
+        for item in s:
+        	ret_str += str(item)
+        	if (i < (len(s) - 1)):
+        		ret_str += ', '
+        	i += 1
+        return ret_str
 
     # Returns a string representation of the object.
     def __repr__(self):
-    	# Stores the index counter
-    	i = 0
-    	# Stores the concatenated string
-    	lines_str = ''
-    	# Concatenates all line in lines into one string
-    	for line in self.lines:
-    	    lines_str += line
-    	    if (i < (len(self.lines) - 1)): # Last line in lines?
-    		lines_str += ', '
-    	    i += 1
-    	return 'Station Name:%s  Zone:%d  Lines:%s' \
-               % (self.name, self.zone, lines_str)
+    	zones_str = self.set_to_string(self.zones)
+    	lines_str = self.set_to_string(self.lines)
+    	return 'Station Name:%s  Is Open?:%r  Zones:%d  Lines:%s' \
+               % (self.name, self.is_open, zones_str, lines_str)
 
     # Returns True if self and other represent stations with the same name
     # and False otherwise.
@@ -50,7 +58,22 @@ class Station:
     def __ne__(self, other):
     	return self != other
 
+    # Sets is_open field to True.
+    def open(self):
+    	isinstance(self, Station)
+    	self.is_open = True
+
+    # Sets is_open field to False.
+    def close(self):
+        isinstance(self, Station)
+        self.is_open = False
+
+    # Adds a zone to the zones field.
+    def add_zone(self, zone):
+    	isinstance(self, Station)
+    	self.zones.add(zone)
+
     # Adds a line to the lines field.
     def add_line(self, line):
     	isinstance(self, Station)
-    	self.lines.append(line)
+    	self.lines.add(line)
