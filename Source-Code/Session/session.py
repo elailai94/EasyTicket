d9879f665d8130fef6fa7792388ce506fbc031e6
Elisha Lai <elisha.lai@outlook.com>
2015-04-30 12:08:56 -0400

    Update session.py

--- Source-Code/Session/session.py
+++ Source-Code/Session/session.py
@@ -33,38 +33,38 @@ class Session:
 
     # Sets the origin station field to station.
     def set_origin_station(self, station):
-	      isinstance(self, Session)
-	      self.__origin_station = station
+	isinstance(self, Session)
+	self.__origin_station = station
 
 
     # Sets the destination station field to station.
     def set_destination_station(self, station):
-	      isinstance(self, Session)
-	      self.__destination_station = station
+	isinstance(self, Session)
+	self.__destination_station = station
 
 
     # Calculates the journey fare for traveling from origin station to
     # destination station.
     def calculate_journey_fare(self):
-    	  isinstance(self, Session)
-    	  if self.__origin_station != None and\
-    	     self.__destination_station != None:
+    	isinstance(self, Session)
+    	if self.__origin_station != None and\
+    	   self.__destination_station != None:
             journey_fare = 4.80
             self.__journey_fare = journey_fare
-    	      return journey_fare
+    	    return journey_fare
     
 
     # Increases the amt of fare paid field by amount.
     def pay_journey_fare(self, amount):
-    	  isinstance(self, Session)
-    	  self.__amt_of_fare_paid += amount
+    	isinstance(self, Session)
+    	self.__amt_of_fare_paid += amount
 
 
     # Returns True if the journey fare has been paid in full. Otherwise,
     # False is return.
     def is_journey_fare_paid(self):
-    	  isinstance(self, Session)
-    	  return self.__amt_of_fare_paid >= self.__journey_fare
+    	isinstance(self, Session)
+    	return self.__amt_of_fare_paid >= self.__journey_fare
 
     #
     def print_ticket(self):
