14865b2618025ebb45786c459a25c8740b18048c
Elisha Lai <elisha.lai@outlook.com>
2015-05-01 14:51:54 -0400

    Update session.py
    Minor changes.

--- Source-Code/Session/session.py
+++ Source-Code/Session/session.py
@@ -14,9 +14,10 @@ import time
 class Session:
 
 
-    'Fields: network, origin_station, destination_station, journey_fare, amt_of_fare_paid'
+    'Fields: network, journey_fares, origin_station, destination_station, journey_fare, amt_of_fare_paid'
     # A Session is an object in which:
     # - network is a Network (network)
+    # - journey_fares is a JourneyFares (journey fares)
     # - origin_station is a Str (origin station)
     # - destination_station is a Str (destination station)
     # - journey_fare is a Float (journey fare from origin station to
@@ -25,8 +26,9 @@ class Session:
 
 
     # Initializes the object.
-    def __init__(self, arg):
-        self.__network = arg
+    def __init__(self, network, journey_fares):
+        self.__network = network
+        self.__journey_fares = journey_fares
         self.__origin_station = None
         self.__destination_station = None
         self.__journey_fare = 0.0
@@ -58,9 +60,51 @@ class Session:
     	isinstance(self, Session)
     	if self.__origin_station != None and\
     	   self.__destination_station != None:
-            journey_fare = 4.80
+            shortest_path = self.__network.find_shortest_path(self.__origin_station, \
+                            self.__destination_station)[1]
+            zones_visited = self.__network.get_zones_visited(shortest_path)
+            min_zone_visited = min(zones_visited)
+            max_zone_visited = max(zones_visited)
+            
+            if min_zone_visited >= 5:
+                journey_fare = self.__journey_fares.lookup_fare('Tier 1')
+            elif min_zone_visited >= 4:
+                if max_zone_visited <= 7:
+                    journey_fare = self.__journey_fares.lookup_fare('Tier 1')
+                else:
+                    journey_fare = self.__journey_fares.lookup_fare('Tier 2')
+            elif min_zone_visited >= 3:
+                if max_zone_visited <= 5:
+                    journey_fare = self.__journey_fares.lookup_fare('Tier 1')
+                else:
+                    journey_fare = self.__journey_fares.lookup_fare('Tier 2')
+            if min_zone_visited >= 2:
+                if max_zone_visited <= 4:
+                    journey_fare = self.__journey_fares.lookup_fare('Tier 1')
+                elif max_zone_visited <= 7:
+                    journey_fare = self.__journey_fares.lookup_fare('Tier 2')
+                else:
+                    journey_fare = self.__journey_fares.lookup_fare('Tier 4')
+            else:
+                if max_zone_visited <= 3:
+                    journey_fare = self.__journey_fares.lookup_fare('Tier 1')
+                elif max_zone_visited <= 5:
+                    journey_fare = self.__journey_fares.lookup_fare('Tier 2')
+                elif max_zone_visited <= 6:
+                    journey_fare = self.__journey_fares.lookup_fare('Tier 3')
+                elif max_zone_visited <= 7:
+                    journey_fare = self.__journey_fares.lookup_fare('Tier 5')
+                else:
+                    journey_fare = self.__journey_fares.lookup_fare('Tier 6')
+
             self.__journey_fare = journey_fare
-    	    return journey_fare
+            return journey_fare
+
+
+    # Returns the journey fare field.
+    def get_journey_fare(self):
+        isinstance(self, Session)
+        return self.__journey_fare
     
 
     # Increases the amt of fare paid field by amount.
@@ -100,6 +144,7 @@ class Session:
         print '------------------------------------------'
         print '|        >> London Underground <<        |'
         print '| Single  ' + '%30s' % (journey_fare_str) + ' |'
+        print '|                                        |'
         print '| ' + time.strftime('%d %b %y') + '%29s' % (self.__destination_station) + ' |'
         print '|     This side up >> Not for resale     |' 
         print '------------------------------------------'
