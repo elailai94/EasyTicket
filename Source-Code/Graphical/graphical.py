da53a77a69dbb14ddd2ba36a3871e2de180b6e4a
Elisha Lai <elisha.lai@outlook.com>
2015-05-05 23:23:25 -0400

    Create graphical.py
    Added graphical.py to current directory.

---
+++ Source-Code/Graphical/graphical.py
@@ -0,0 +1,17 @@
+#==============================================================================
+# EasyTicket
+#
+# @description: Module for providing function to run the graphical version of
+#   the EasyTicket system
+# @author: Elisha Lai
+# @version: 1.1 04/05/2015
+#==============================================================================
+
+# Graphical module (graphical.py)
+
+from ticket_machine import TicketMachine
+
+# Runs the graphical version of the system.
+def graphical_version():
+	print "This is the graphical version of the EasyTicket system."
+	#Tkinter.mainloop()
