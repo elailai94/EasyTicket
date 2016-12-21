59062f265743fe888781d59bb303181ce0ea6d40
Ah Hoe Lai <ahlai@uwaterloo.ca>
2016-12-21 01:24:22 -0500

    minor changes

--- source-code/main.py
+++ source-code/main.py
@@ -7,13 +7,16 @@
 # =============================================================================
 
 from tkinter import Tk
+from tkinter import Menu
+
 from model import Model
 
 
 def main():
     # Creates the window
     window = Tk()
-    print(window)
+    window.title("Easy Ticket")
+
     # Creates a model and initializes it
     model = Model()
 
@@ -22,7 +25,16 @@ def main():
     # Creates all the views and tells them about model and controller
 
     # Tells model about all the views
-    model.add_observer()
+    #model.add_observer()
+    
+    # Loop
+    window.mainloop()
+
+def make_menu_bar(window):
+    menu_bar = Menu(window)
+    
+    file_menu = Menu(menu_bar)
+    file_menu.add_cascade(label="File", menu=file_menu)
 
 if __name__ == '__main__':
     main()
